6c6a0ef78072efb58974abd667a60bcd90e1e168
rmtrigo <rmontrigo@gmail.com>
2015-10-13 21:02:17 -0700

    Earned Built in Annotations badge

---
+++ src/com/teamtreehouse/deprecated/Seller.java
@@ -0,0 +1,16 @@
+package com.teamtreehouse.deprecated;
+
+import java.awt.*;
+import java.util.List;
+
+public class Seller {
+    @Deprecated
+    public void placeAdInNewspaper(String adText, int numDays) {
+        // Do all of the old school things
+    }
+
+    public void postInCraigsList(String adText, List<Image> image) {
+        // Do all of the new school stuff
+
+    }
+}
---
+++ src/com/teamtreehouse/override/Cheese.java
@@ -0,0 +1,11 @@
+package com.teamtreehouse.override;
+
+/**
+ * Created by RamonTrigo on 15-10-13.
+ */
+public class Cheese {
+    @Override
+    public String toString() {
+        return "String cheese!";
+    }
+}
